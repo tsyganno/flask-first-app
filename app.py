78d0dbba84aa2026c196199637a98abe566a7023
tsyganno <tsyganlee@gmail.com>
2022-07-30 16:47:15 +0800

    start server

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 from flask import Flask, render_template
 
+from game_of_life import GameOfLife
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
+    GameOfLife(25, 25)
     return render_template('index.html')
 
 
+@app.route('/live/')
+def live():
+    y = GameOfLife()
+    y.form_new_generation()
+    return render_template('live.html', x=y.world)
+
+
 if __name__ == '__main__':
+    app.debug = True
     app.run(host='127.0.0.1', port=5000)
